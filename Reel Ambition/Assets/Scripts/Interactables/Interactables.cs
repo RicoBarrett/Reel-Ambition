e0a4467602f4c1f7f399f6853cfb2c12bf413fdf
RicoBarrett <st20300362@outlook.cardiffmet.ac.uk>
2025-01-25 16:14:57 +0000

    Added interactables

---
+++ Reel Ambition/Assets/Scripts/Interactables/Interactables.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactables : MonoBehaviour
+{
+    private Rigidbody2D playerBody;
+    private PlayerMovement playerMovement;
+    private Manager manager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBody = GameObject.Find("Player").GetComponent<Rigidbody2D>();
+        playerMovement = GameObject.Find("Player").GetComponent<PlayerMovement>();
+        manager = GameObject.Find("Manager").GetComponent<Manager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (this.CompareTag("Soda"))
+            {
+                playerBody.velocity = new Vector2(playerBody.velocity.x, playerBody.velocity.y + 15);
+                Destroy(gameObject);
+            }
+
+            if (this.CompareTag("Coffee"))
+            {
+                StartCoroutine(CoffeeEffect());
+            }
+
+            if (this.CompareTag("Flower"))
+            {
+                manager.flowerPicked = true;
+                Destroy(gameObject);
+            }
+
+            if (this.CompareTag("Secretary"))
+            {
+                if (manager.flowerPicked) 
+                {
+                    Debug.Log("Let's fuck!!");
+                    manager.flowerPicked = false;
+                }
+                else
+                {
+                    Debug.Log("Ew");
+                }
+            }
+        }
+    }
+
+    IEnumerator CoffeeEffect()
+    {
+        Time.timeScale = 0.8f;
+        playerMovement.speed = 6f;
+        Destroy(gameObject);
+        yield return new WaitForSeconds(5f);
+        Time.timeScale = 1f;
+        playerMovement.speed = 5f;
+    }
+}
