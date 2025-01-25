183932fe38560dbfc69d2d6c0f9624a1e294cd41
RicoBarrett <st20300362@outlook.cardiffmet.ac.uk>
2025-01-25 13:56:26 +0000

    Added basic player functionality

---
+++ Reel Ambition/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    //You can set the player as the target to be follow
+    public Transform player;
+
+    void start()
+    {
+  
+    }
+
+    //makes sure the camera cannot leave the bounds of position 7 and -7 on the x axis 
+    void Update()
+    {
+        if (player.transform.position.x <= 2.1 & player.transform.position.x >= -2.1)
+        {
+            transform.position = player.transform.position + new Vector3(0, 1, -5);
+        }
+        else
+        {
+            transform.position = new Vector3(transform.position.x, player.transform.position.y, player.transform.position.z) + new Vector3(0, 1, -5);
+        }
+    }
+
+}
